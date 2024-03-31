7b89e138188113fe6d9075e9b37d0e05100cc24a
墨七 <mo7@mo7.cc>
2024-03-31 20:11:08 +0800

    发布

--- trade/CountIndex.go
+++ trade/CountIndex.go
@@ -20,7 +20,7 @@ type TradeCandle struct {
 }
 
 func (_this *TradeObj) FormatCandle() {
-	TradeCandleList := []TradeCandle{}
+	nowTradeCandleList := []TradeCandle{}
 
 	for _, v := range _this.NowCandle {
 		EMA100 := mTalib.ClistNew(mTalib.ClistOpt{
@@ -33,6 +33,34 @@ func (_this *TradeObj) FormatCandle() {
 			EMA100: EMA100,
 		}
 
-		TradeCandleList = append(TradeCandleList, newDataObj)
+		nowTradeCandleList = append(nowTradeCandleList, newDataObj)
 	}
+
+	offsetTradeCandleList := []TradeCandle{}
+	for _, v := range _this.OffsetCandle {
+		EMA100 := mTalib.ClistNew(mTalib.ClistOpt{
+			KDList: _this.OffsetCandle,
+			Period: 100,
+		}).EMA().ToStr()
+
+		newDataObj := TradeCandle{
+			TypeKd: v,
+			EMA100: EMA100,
+		}
+		offsetTradeCandleList = append(offsetTradeCandleList, newDataObj)
+	}
+
+	nowEma := nowTradeCandleList[len(nowTradeCandleList)-1].EMA100
+	offsetEma := offsetTradeCandleList[len(offsetTradeCandleList)-1].EMA100
+
+	Dir := 0
+	if nowEma > offsetEma {
+		Dir = -1
+	}
+	if nowEma > offsetEma {
+		// 上涨趋势
+		Dir = 1
+	}
+
+	global.Log.Println("当前信号指标为:", Dir)
 }
