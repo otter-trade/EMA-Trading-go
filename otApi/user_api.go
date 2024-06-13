4ad2ff075f05a9a64faefe49e92abb4e64fc5c52
mo7 <mo7@mo7.cc>
2024-06-13 15:06:58 +0800

    xg

--- otApi/user_api.go
+++ otApi/user_api.go
@@ -14,8 +14,8 @@ var strategyID = "e395ffe2-3a99-4072-abfc-0da940f32f26"
 
 func StartUserApi() {
 	fmt.Println("StartUserApi")
-	GetMockNameList()
-	// GetHistoryStatus()
+	// GetMockNameList()
+	GetHistoryStatus()
 }
 
 // 获取 获取历史持仓列表
@@ -42,7 +42,7 @@ func GetHistoryStatus() {
 	data := map[string]any{
 		"StrategyID": strategyID, // 策略ID 每个策略唯一，当前用户的当前策略
 		"RunType":    1,          // 1：回测类型 2：预览类型 3：线上类型
-		"MockName":   MockName,   // 策略ID 每个策略唯一，当前用户的当前策略
+		"MockName":   MockName,   // MockName 本次回测的名字
 	}
 
 	res, err := fetch.Post(fetch.Opt{
